dcaa7959dea2dbbdefa3985d95bda0cc0ee14760
Johman Castillo <johmancastillo@gmail.com>
2023-07-01 20:45:34 -0500

    proyecto 2-2

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuIcon = document.getElementById('menu-icon');
+const menu = document.getElementById('menu');
+
+menuIcon.addEventListener('click', function () {
+    menu.classList.toggle('active');
+});
